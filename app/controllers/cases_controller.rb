71a1c5508c718ca4e0c6ce08b745cd59ad5cd399
shichao <315046509@qq.com>
2014-06-23 17:13:00 +0800

    提交

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,9 +1,5 @@
 class CasesController < ApplicationController
   def index
-    @roll = Case.all.order("id desc").page(params[:page]).per(10)
-  end
-
-  def show
-    @roll = Case.all.order("id desc").page(params[:page]).per(10)
+    @roll = Case.all.order("id desc").page(params[:page]).all
   end
 end
