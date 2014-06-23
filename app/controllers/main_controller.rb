95a8d6170424c7eca26cc39835e7129a4975f853
shichao <315046509@qq.com>
2014-06-23 09:37:26 +0800

    提及哦傲

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
   def index
     # 最新资讯
-    @journalisms = Journalism.order_ct_desc.page(params[:page]).per(15)
+    @journalisms = Journalism.order_ct_desc.page(params[:page]).per(6)
     # 经典案例
-    @roll = Case.order_ct_desc.page(params[:page]).per(12)
+    @roll = Case.order_ct_desc.page(params[:page]).per(6)
   end
 end
